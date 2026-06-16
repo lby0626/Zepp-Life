bc6981426f4f67549553715b0072ebd005594bf1
likeLBY <3255962845@qq.com>
2026-06-16 18:39:10 +0800

    chore: auto-open browser after starting Flask

--- archive/start.bat
+++ archive/start.bat
@@ -6,4 +6,4 @@ timeout /t 1 >nul
 pip install -r requirements.txt -q 2>nul
 start /min python app.py
 timeout /t 2 >nul
-echo Done! Open http://127.0.0.1:5800 in your browser
+start http://127.0.0.1:5800
